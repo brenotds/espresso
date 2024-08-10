fd44e45b470b4c821be5b98e844068c643d5574d
Breno Tardelli <111755954+brenotds@users.noreply.github.com>
2024-08-10 21:58:41 +0100

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function initAnimeScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+
+  if (sections.length) {
+    const windowSize = window.innerHeight * 0.6;
+
+    function animeScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowSize < 0;
+        if (isSectionVisible) {
+          section.classList.add("anime");
+        }
+      });
+    }
+    animeScroll();
+    window.addEventListener("scroll", animeScroll);
+  }
+}
+initAnimeScroll();
+
+function initImageSwap() {
+  const image = document.getElementById("image-bg");
+  const images = ["/img/img1.jpg", "/img/img2.jpg", "/img/img3.jpg"];
+
+  setInterval(function () {
+    const random = Math.floor(Math.random() * 3);
+    image.src = images[random];
+  }, 3000);
+}
+initImageSwap();
